fab489d7e92676031df6cb5c1164e51fdf545f42
Daniel <danieldoleski02@gmail.comm>
2024-02-02 22:08:02 -0300

    Adicionando Entidades

---
+++ src/Omnitrix/RegrasDeNegocios/Entidades/Cliente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegrasDeNegocios.Entidades
+{
+    public class Cliente : EntidadeBase
+    {
+        public string Nome { get; set; }
+
+        public string CPF { get; set; }
+
+        public int Idade { get; set; }
+
+        public string Endereco {  get; set; }
+
+        public string Email { get; set;}
+
+        
+    }
+}
---
+++ src/Omnitrix/RegrasDeNegocios/Entidades/Ingrediente.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegrasDeNegocios.Entidades
+{
+    public class Ingrediente : EntidadeBase
+    {
+        public string Pao { get; set; }
+
+        public string Hamburguer { get; set; }
+
+        public string Maionese { get; set; }
+
+        public string Alface { get; set; }
+
+        public string Bacon {  get; set; }
+
+        public string Cebola { get; set; }
+
+        public string Ovo { get; set; }
+
+        public string Tomate { get; set; }
+
+        public string Queijo { get; set; }
+
+        public string Picles { get; set; }
+
+
+
+
+    }
+}
