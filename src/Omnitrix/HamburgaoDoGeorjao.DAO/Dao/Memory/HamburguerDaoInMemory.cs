c10b42cc35770b42fe326265f7166213947a21be
ElderAndrade <elder_andrade@estudante.sesisenai.org.br>
2024-02-09 00:26:45 -0300

    correção pedidioClienteVo, iniciado hamburguer

--- src/Omnitrix/HamburgaoDoGeorjao.DAO/Dao/Memory/HamburguerDaoInMemory.cs
+++ src/Omnitrix/HamburgaoDoGeorjao.DAO/Dao/Memory/HamburguerDaoInMemory.cs
@@ -10,6 +10,8 @@ namespace HamburgaoDoGeorjao.DAO.Dao.Memory
 {
     public class HamburguerDAoInMemory : IHamburguerDao
     {
+        public List<HamburguerVo> Hamburguers { get; set; }
+
         public Task AtualizarRegistro(HamburguerVo objetoParaAtualizar)
         {
             var atualizarId =
---
+++ src/Omnitrix/HamburgaoDoGeorjao.DAO/ValueObjects/PedidoClienteVo.cs
@@ -0,0 +1,20 @@
+﻿using HamburgaoDoGeorjao.DAO.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HamburgaoDoGeorjao.DAO.ValueObjects
+{
+    public class PedidoClienteVo : EntidadeBaseVo
+    {
+        public PedidoClienteVo() { }
+        public int PedidoClienteId { get; set; }
+        public DateTime DataSolicitacao { get; set; }
+        public DateTime? DataPreparacao { get; set; }
+        public DateTime? DataSaidaEntrega { get; set; }
+        public DateTime? DataFinalizacaoEntrega { get; set; }
+        public int HamburguerId { get; set; }
+    }
+}
