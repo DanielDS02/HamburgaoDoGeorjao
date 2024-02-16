957d928670c72e967c66e086c4347d2b011939ed
Daniel <danieldoleski02@gmail.comm>
2024-02-16 13:59:17 -0300

    Models(Login)

--- src/Omnitrix/Omnitrix.Mvc/Models/Login.cs
+++ src/Omnitrix/Omnitrix.Mvc/Models/Login.cs
@@ -1,6 +1,14 @@
-﻿namespace HamburgaoDoGeorjao.Mvc.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HamburgaoDoGeorjao.Mvc.Models
 {
     public class Login
     {
+        [Required]
+        [EmailAddress(ErrorMessage = "Entre com um email válido")]
+        public string email { get; set; }
+
+        [Required]
+        public string password { get; set; }
     }
 }
