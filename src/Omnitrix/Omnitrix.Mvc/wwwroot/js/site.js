8025901055e7e1f1993c4c253a8b135ef97f00f7
Brian Batista <brian.bti2004@gmail.com>
2024-02-15 23:04:39 -0300

    Alteração view (ênfase em img e layout).

--- src/Omnitrix/Omnitrix.Mvc/wwwroot/js/site.js
+++ src/Omnitrix/Omnitrix.Mvc/wwwroot/js/site.js
@@ -1,4 +1,11 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+// Função para mostrar o modal ao clicar no carrinho
+function abrirModalCarrinho() {
+    var myModal = new bootstrap.Modal(document.getElementById('modalCarrinho'));
+    myModal.show();
+}
+
+// Adiciona um ouvinte de evento de clique ao elemento do carrinho
+document.getElementById('carrinhoLink').addEventListener('click', abrirModalCarrinho);
