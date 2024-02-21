9ab74a34739cf42e04e5ed1194ef1d8caadc2e05
Daniel <danieldoleski02@gmail.comm>
2024-02-21 19:48:36 -0300

    Atulização Program MVC

--- src/Omnitrix/HamburgaoDoGeorjao/Program.cs
+++ src/Omnitrix/HamburgaoDoGeorjao/Program.cs
@@ -1,13 +1,54 @@
+using HamburgaoDoGeorjao.DAO.Regras;
+using RegrasDeNegocios.Regras;
+using RegrasDeNegocios.Serviços;
+using HamburgaoDoGeorjao.DAO.Dao;
+
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var logger = new LoggerConfiguration()
+    .ReadFrom.
+    Configuration(builder.Configuration)
+    .CreateLogger();
+
+Log.Logger = logger;
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.Configure<ConnectionStrings>(builder.Configuration.GetSection("ConnectionStrings"));
+
+var connectionStrings = builder.Configuration.GetSection("ConnectionStrings").GetValue<string>("Master");
+
+Console.WriteLine(connectionStrings);
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("Testes"));
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+// Criação objetos acesso a dados
+builder.Services.AddScoped<IHamburguerDao, HamburguerRepository>();
+builder.Services.AddScoped<IClienteDao, ClienteRepository>();
+builder.Services.AddScoped<IPedidoDao, PedidoRepository>();
+
+//Criação objetos de serviço
+builder.Services.AddScoped<IPizzaService, PizzaService>();
+builder.Services.AddScoped<IClienteService, ClienteService>();
+builder.Services.AddScoped<IPedidoService, PedidoService>();
+
+builder.Services.AddLogging(configure =>
+{
+    configure.AddConsole();
+    configure.AddSerilog();
+});
+
+
 var app = builder.Build();
+
+app.Logger.LogInformation("Ola eu sou informação");
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -16,9 +57,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
+
 
 app.MapControllers();
 
